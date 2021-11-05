394d508c36136213803257db95418692d6b07b68
wkargul <wojciech.kargul@gmail.com>
2021-11-05 14:03:19 +0100

    third refactoring and if break added

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use models::{App, Contributor, KeyContributor};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    //try to parse cli arguments into App struct.
+    //try to parse cli arguments (<language> and <project_count>) into App struct.
     let args = App::from_args();
     //define github_api_client
     let github_api_client = octocrab::Octocrab::builder()
@@ -15,6 +15,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     //assemble query to GitHub
     let query = &*("language:".to_owned() + args.language.as_str());
     //fetch page of records
+    //by default setup in GitHubAPI it returns 30 records per page
     let mut page = github_api_client
         .search()
         .repositories(query)
@@ -23,47 +24,56 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         .send()
         .await?;
     //number of processed repositories
-    let mut processed_repos = 0u32;
+    let mut processed_projects = 0u32;
+    //switch for the process
+    let mut continue_analysis: bool = true;
 
-    let mut switch: bool = true;
-    while switch {
+    while continue_analysis {
+        //loop through records in returned page of records
         for project in &page {
-            if processed_repos < args.project_count {
+            //check if total number of processed projects is not greater than <project_count>
+            if processed_projects < args.project_count {
+                //get contributors as Vec<Contributor>
                 match github_api_client
                     .get::<Vec<Contributor>, &reqwest::Url, ()>(&project.contributors_url, None::<&()>)
                     .await {
-                    Ok(contributors) => {
-                        match analyze_contributors(contributors) {
-                            None => {}
-                            Some(key_contributor) => {
-                                print_stdout(&project.name, key_contributor.user, key_contributor.percentage);
-                            }
-                        };
-                    }
+                    Ok(contributors) => { process(contributors, &project.name) }
                     Err(error) => println!("{:#?}", error)
                 }
-                processed_repos += 1;
+                processed_projects += 1;
             } else {
-                switch = false;
+                continue_analysis = false;
                 break;
             }
         }
 
-        if switch {
+        if continue_analysis {
+            //get next page of records
             page = match github_api_client
                 .get_page::<octocrab::models::Repository>(&page.next)
                 .await? {
                 Some(next_page) => next_page,
                 None => break,
             }
+        } else {
+            break;
         }
     }
-    println!("\n{} projects have been analyzed", processed_repos);
+    println!("\n{} projects have been analyzed", processed_projects);
 
     Ok(())
 }
 
-fn analyze_contributors(contributors: Vec<Contributor>) -> Option<KeyContributor> {
+fn process(contributors: Vec<Contributor>, project_name: &str) {
+    match analyse_contributors(contributors) {
+        None => {}
+        Some(key_contributor) => {
+            print_stdout(project_name, key_contributor.user, key_contributor.percentage);
+        }
+    }
+}
+
+fn analyse_contributors(contributors: Vec<Contributor>) -> Option<KeyContributor> {
     //collect contributors in HashMap -> <key: login, value: contributions>
     let contr_map: HashMap<String, f64> = contributors
         .iter()
@@ -83,9 +93,10 @@ fn analyze_contributors(contributors: Vec<Contributor>) -> Option<KeyContributor
 
 fn bus_factor_check(login: &str, contributions: &f64, total_project_contributions: f64) -> Option<KeyContributor> {
     let percentage = contributions / total_project_contributions;
-    match percentage >= 0.75 {
-        true => Some(KeyContributor::new(login.to_string(), percentage)),
-        false => None
+    if percentage >= 0.75 {
+        Some(KeyContributor::new(login.to_string(), percentage))
+    } else {
+        None
     }
 }
 
